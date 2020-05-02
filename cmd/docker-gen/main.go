a7d6c660c5369ffe241a7d4893bbb0ff92e82f86
Joel Linn <jl@conductive.de>
2020-05-02 16:39:36 +0200

    Fix exit(2) on SIGHUP before fully initialized. Fixes https://github.com/jwilder/docker-gen/issues/244

--- cmd/docker-gen/main.go
+++ cmd/docker-gen/main.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"sync"
+	"syscall"
 
 	"github.com/BurntSushi/toml"
 	"github.com/JoelLinn/docker-gen"
@@ -111,6 +113,10 @@ func initFlags() {
 }
 
 func main() {
+	// SIGHUP is used to trigger generation but go programs call os.Exit(2) at default.
+	// Ignore the signal until the handler is registered:
+	signal.Ignore(syscall.SIGHUP)
+
 	initFlags()
 
 	if version {
